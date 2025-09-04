dcbabc8ffbee57b70b63486b534d2f4fee78968e
R0rro06 <rorohidalgo35@gmail.com>
2025-09-04 14:52:32 -0400

    se agrega un menu principal

--- agenda.py
+++ agenda.py
@@ -1,5 +1,6 @@
 #Agenda
 contacto = []
+opciones = 0
 
 def agregar_contacto(nombre, telefono):
     contacto.append({"nombre" : nombre,"telefono" : telefono})
@@ -7,8 +8,8 @@ def agregar_contacto(nombre, telefono):
 def listar_contactos():
     print("\nlista de contactos : ")
     for c in contacto:
-        print(f"{c['nombre']} - [{c}'telefono']")
-
+        print(f"{c['nombre']} - [{c['telefono']}]")
+        
 def buscar_contacto():
     for c in contacto:
         if c ['nombre'].lower() == nombre.lower():
@@ -25,5 +26,33 @@ listar_contactos()
 #print("Contacto agregado")
 
 
-nombre_buscar = input("nombre a buscar: ")
-buscar_contacto(nombre_buscar)
+#nombre_buscar = input("nombre a buscar: ")
+#buscar_contacto(nombre_buscar)
+
+while opciones != 4:
+    print("-----MENU-----")
+    print("1-Agregar contacto")
+    print("2-Listar contacto")
+    print("3-buscar contacto")
+    print("4-Salir del programa")
+    opciones = int(input("Escoja una de las opciones : "))
+    if opciones == 1:
+        nombre = input("Ingrese el nombre")
+        telefono = input("Ingrese el numero de telefono : ")
+        agregar_contacto(nombre,telefono)
+
+    elif opciones == 2:
+        listar_contactos()
+
+    elif opciones == 3:
+        nombre_buscar = input("ingrese el nombre que desea buscar: ")
+        buscar_contacto(nombre_buscar)
+
+    elif opciones == 4:
+    
+        print("Hasta luego")
+
+    else:
+        print("Ingrese una opcion valida")
+
+
