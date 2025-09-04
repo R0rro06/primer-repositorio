eaf16c349e57caba4d6d061d607cf3ef36b97d32
R0rro06 <rorohidalgo35@gmail.com>
2025-09-04 14:11:12 -0400

    se agrega funcion para listar contactos

--- agenda.py
+++ agenda.py
@@ -4,6 +4,12 @@ contacto = []
 def agregar_contacto(nombre, telefono)
     contacto.append({"nombre" : nombre,"telefono" : telefono})
 
+def listar_contactos()
+    print("\nlista de contactos : ")
+    for c in contacto:
+        print(f"{c['nombre']} - [{c}'telefono']")
+
+listar_contactos()
 nombre = input("Ingrese el nombre :" )
 telefono = input("Ingrese el telefono :")
 agregar_contacto(nombre, telefono)
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # Agenda de contactos 
 proyecto simple de consola para registrar, buscar y listar contactos usando python
+## Agenda.py 
+se creo la funcion de agregar contacto
+se creo la funcion para listar contactos
